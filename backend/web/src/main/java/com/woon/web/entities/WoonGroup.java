10051553736816e52fa2d4289bbc061c27bd7239
laevus.j <laevus.j@gmail.com>
2019-07-11 20:55:37 +0900

    2019-07-11/ 20:55 / 그룹 유저 테이블 연관관계 설정

--- backend/web/src/main/java/com/woon/web/entities/WoonGroup.java
+++ backend/web/src/main/java/com/woon/web/entities/WoonGroup.java
@@ -4,6 +4,12 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AccessLevel;
@@ -23,18 +29,27 @@ import lombok.ToString;
 @ToString
 @Table(name = "groups")
 public class WoonGroup implements Serializable {
+    
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @OneToMany //(WoonGroup <->WoonGroupDetail)
     private Long groupno;
-    @Column(name = "group_name")
-    private String groupName;
-    @Column(name = "group_info")
-    private String groupInfo;
+    
+    @Column(name = "group_name", nullable = false) private String groupName;   
+    @Column(name = "group_info", nullable = false) private String groupInfo;
+    //table 연관 관계 설정
+    //(WoonGroup <-> WoonUser)    
+    @ManyToOne
+    @JoinColumn(name="uno")
+    private WoonUser uno;
+    
 
     @Override
     public String toString(){
         return String.format("GROUP NO: %d\n"
                             +"GROUP NAME: %s\n"
-                            +"GROUP INTRO: %s\n"
+                            +"GROUP INFO: %s\n"
         , groupno, groupName, groupInfo);
     }
     @Builder
