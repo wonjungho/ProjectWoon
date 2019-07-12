d4f094dc9a9428899e2ed21f4c41bda39c649688
WonJungHo <1jungho0809@gmail.com>
2019-07-12 19:43:24 +0900

    20190712 / 19:43 WebRTC TEST

---
+++ frontend/src/components/Video.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import VideoCall from '../helpers/simple-peer'
+// import '../styles/video.css'
+import io from 'socket.io-client'
+
+class Video extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            localStream:{},
+            remoteStreamUrl:'',
+            streamUrl:'',
+            initiator: false,
+            peer:{},
+            full:false,
+        }
+
+    }
+    videoCall = new VideoCall()
+    componentDidMount(){
+        const socket = io(process.env.REACT_APP_SIGNALING_SERVER)
+        const component = this
+        this.setState({socket})
+        const { roomId } = this.props.match.params
+        this.getUserMedia().then(() => {
+            socket.emit('join', { roomId:roomId })
+        })
+        socket.on('init', ()=>{
+            component.setState({ initiator: true})
+        })
+        socket.on('ready', ()=>{
+            component.enter(roomId)
+        })
+        socket.on('desc', (data) => {
+            if (data.type ==='offer' && component.state.initiator) return
+            if (data.type === 'answer' && !component.state.initiator) return
+            component.call(data) 
+        })
+        socket.on('disconnected', () => {
+            component.setState({ initiator: true })
+        })
+        socket.on('full', () => {
+            component.setState({full:true})
+        })
+
+    }
+    getUserMedia(cb){
+        return new Promise((resolve, reject) => {
+            navigator.getUserMedia = navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+            const op = {
+                video: {
+                    width: { min: 160, ideal: 640, max: 1280 },
+                    height: { min: 120, ideal: 360, max: 720 },
+                },
+                audio: true 
+            }
+            navigator.getUserMedia(op, stream => {
+                this.setState({ streamUrl: stream, localStream: stream })
+                this.localVideo.srcObject = stream
+                resolve()
+            }, () => { }) 
+        })
+    }
+    enter = (roomId) => {
+        const peer = this.videoCall.init(this.state.localStream, this.state.initiator)
+        peer.on('signal', data => {
+            const signal = {
+                room: roomId,
+                desc: data
+            }
+            this.state.socket.emit('signal', signal)
+        })
+        peer.on('stream', stream => {
+            this.remoteVideo.srcObject = stream
+        })
+        peer.on('error', function (err) { 
+            console.log(err)
+        })
+
+    }
+    call = (otherId) => {
+        this.videoCall.connect(otherId)
+    }
+    renderFull = () => {
+        if(this.state.full){
+            return 'The room is full'
+        }
+    }
+    render(){
+        return( 
+            <div className="video-wrapper">
+                <div className="local-video-wrapper">
+                    <video autoPlay id="localVideo" muted ref={video => (this.localVideo = video)}></video>
+                </div>
+                <video autoPlay id="remoteVideo"  ref={video => (this.remoteVideo = video)}></video>
+                {this.renderFull()}
+            </div>
+        )
+    }
+}
+
+export default Video
--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,16 +1,22 @@
 import React,{Component} from 'react';
 import {Grommet} from 'grommet'
-import {grommet, dark} from 'grommet/themes'
+import {grommet} from 'grommet/themes'
 import WoonGrid  from '../components/WoonGrid'
+import { BrowserRouter, Route } from 'react-router-dom';
+import {GoToRoomInput} from '../components/GoToRoomInput'
+import Video from '../components/Video'
 import './test.css'
 
 class App extends Component{
   render(){
     return(
+      <BrowserRouter>
          <Grommet full theme={grommet}>
-        <WoonGrid>
-        </WoonGrid>
+        {/* <WoonGrid/> */}
+        <Route path="/" exact component={GoToRoomInput}/>
+        <Route path="/:roomId" exact component={Video}/>
       </Grommet>
+      </BrowserRouter>
     );
   }
 }
