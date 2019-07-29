6e61ee680431033170bb1e2db6b6ee7e99b41cb6
laevus.j <laevus.j@gmail.com>
2019-07-29 14:01:35 +0900

    2019-07-29/ 14:00 / 그룹컨트롤, 조인 그룹 레포

--- backend/web/src/main/java/com/woon/web/repositories/WoonJoinGroupRepository.java
+++ backend/web/src/main/java/com/woon/web/repositories/WoonJoinGroupRepository.java
@@ -13,9 +13,11 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface WoonJoinGroupRepository extends CrudRepository<WoonJoinGroup, Long> {
+    //그룹 탈퇴 
     @Query(value="DELETE FROM tbl_joingroups WHERE uno=?1 and groupno=?2",nativeQuery=true)
     void deleteByUnoAndGroupno(Long uno, Long groupno);
     
+    //현재 사용자가 해당 그룹의 리더인지 판단
     @Query(value="SELECT GROUP_leader FROM tbl_joingroups jg WHERE jg.uno=?1 and jg.groupno=?2",nativeQuery=true)
 	public String checkLeader(Long uno, Long groupno); 
 }
