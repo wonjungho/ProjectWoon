cc0b7fedd21ae3b1f37da797460f01f130790ff5
WonJungHo <1jungho0809@gmail.com>
2019-07-30 15:51:31 +0900

    20190730/15:51 메인페이지 CSS 완료

--- frontend/src/components/WoonIndexPage.js
+++ frontend/src/components/WoonIndexPage.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react'
-import { Video,Box,Text, Heading,Button } from 'grommet'
+import { Video, Box, Text, Heading, Button } from 'grommet'
 import video from '../assets/images/video.mp4'
+import Typer from './WoonTyping'
 import '../assets/css/bigvideo.css'
 
 
 class WoonIndexPage extends Component {
-    constructor(props, context) {
-        super(props, context);
-
+   
+    constructor(props) {
+        super(props);
     }
     render() {
         return (
             <>
-            <Video autoPlay="true" mute="true" loop="true" controls="false" className="bigvideo">
-                <source src={video} type="video/mp4"></source>
-            </Video>
-            <Box background={{ color: "dark-1", opacity:"strong"}} className="content2"></Box>
-            <Box className="content">
-                <Heading size="xlarge" color="white" textAlign="center">Hello</Heading>
-                <Heading size="large" color="white">The World Best Cowork TooL</Heading>
-            </Box>
+                <Video autoPlay="true" mute="true" loop="true" controls="false" className="bigvideo">
+                    <source src={video} type="video/mp4"></source>
+                </Video>
+                <Box background={{ color: "dark-1", opacity: "strong" }} className="content2" border={{ color: "black", size: "xlarge" }}></Box>
+                <Box className="content" pad={{bottom:"20%",right:"20%"}}>
+                    <Typer
+                    heading={'Hello'}
+                    dataText={["This is Woon","어서와! 운은 처음이지?","Cowork with Woon","WorkingTogether!","우리 일같이할래?","Study with Woon","StudyTogether!","우리 공부같이할래?"]}
+            
+                    />
+                </Box>
             </>
         )
     }
---
+++ frontend/src/components/WoonTyping.js
@@ -0,0 +1,62 @@
+import React,{Component} from 'react'
+import "../assets/css/typing.css"
+import "../assets/css/bigvideo.css"
+import { Heading,Box } from 'grommet';
+
+class Typer extends Component {
+    static defaultProps = {
+      heading: '',
+      dataText: []
+    }
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        text: '',
+        isDeleting: false,
+        loopNum: 0,
+        typingSpeed: 150
+      }
+    }
+  
+    componentDidMount() {
+      this.handleType();
+    }
+    handleType = () => {
+      const { dataText } = this.props;
+      const { isDeleting, loopNum, text, typingSpeed } = this.state;
+      const i = loopNum % dataText.length;
+      const fullText = dataText[i];
+  
+      this.setState({
+        text: isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1),
+        typingSpeed: isDeleting ? 30 : 150
+      });
+  
+      if (!isDeleting && text === fullText) {
+        
+        setTimeout(() => this.setState({ isDeleting: true }), 500);
+        
+      } else if (isDeleting && text === '') {
+        
+        this.setState({
+          isDeleting: false,
+          loopNum: loopNum + 1
+        });
+        
+      }
+  
+      setTimeout(this.handleType, typingSpeed);
+    };
+  
+    render() {    
+      return (
+        <Heading size="large" color="white" className="typer">{this.props.heading}&nbsp;<br/>
+          <span>{ this.state.text}</span>
+          <span id="cursor"/>
+        </Heading>
+      );
+      
+    }
+  }
+export default Typer   
